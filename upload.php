a365fa181e0f4fac5cc15f678dee2ed83bd4d1cf
cozymewa <mlodymewa.biznes@gmail.com>
2024-02-26 12:21:18 +0100

    .

--- upload.php
+++ upload.php
@@ -1,11 +1,17 @@
 <?php
 if(!empty($_POST)) {
-
+    //coś przyszło postem
     $postTitle = $_POST['postTitle'];
     $postDescription = $_POST['postDescription'];
-
-
+    //wgrywanie pliku
+    //zdefiniuj folder docelowy
     $targetDirectory = "img/";
+    //użyj oryginalnej nazwy pliku
+    $fileName = $_FILES['file']['name'];
+    //przesuń plik z lokalizacji tymczasowej do docelowej
+    move_uploaded_file($_FILES['file']['tmp_name'], $targetDirectory.$fileName);
+
+    //dopisz posta do bazy
 }
 ?>
 
@@ -14,9 +20,21 @@ if(!empty($_POST)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dodaj nowy post</title>
 </head>
 <body>
-    <form> action="upload.php" method="post" enctype="multipart/form-data">
+    <!--musi byc multipart/form-data dla transferu plików -->
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="postTitleInput">Tytuł posta:</label>
+        <input type="text" name="postTitle" id="postTitleInput">
+        <br>
+        <label for="postDescriptionInput">Opis posta:</label>
+        <input type="text" name="postDescription" id="postDescriptionInput">
+        <br>
+        <label for="fileInput">Obrazek:</label>
+        <input type="file" name="file" id="fileInput">
+        <br>
+        <input type="submit" value="Wyślij!">
+    </form>
 </body>
 </html>
