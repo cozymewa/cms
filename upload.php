7cbdce64ef3304d39c46eb7a2ba2ce20d61f3705
cozymewa <mlodymewa.biznes@gmail.com>
2024-02-26 12:20:08 +0100

    new

---
+++ upload.php
@@ -0,0 +1,22 @@
+<?php
+if(!empty($_POST)) {
+
+    $postTitle = $_POST['postTitle'];
+    $postDescription = $_POST['postDescription'];
+
+
+    $targetDirectory = "img/";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form> action="upload.php" method="post" enctype="multipart/form-data">
+</body>
+</html>
